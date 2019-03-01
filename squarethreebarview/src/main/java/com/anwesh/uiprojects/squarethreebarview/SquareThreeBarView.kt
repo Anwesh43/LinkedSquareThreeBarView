104499966f7899a6cf7fde18b4088ebb51e03751
Anwesh43 <anweshthecool0@gmail.com>
2019-03-02 01:35:27 +0530

    adding constants and importing libraries

--- squarethreebarview/src/main/java/com/anwesh/uiprojects/squarethreebarview/SquareThreeBarView.kt
+++ squarethreebarview/src/main/java/com/anwesh/uiprojects/squarethreebarview/SquareThreeBarView.kt
@@ -3,3 +3,22 @@ package com.anwesh.uiprojects.squarethreebarview
 /**
  * Created by anweshmishra on 02/03/19.
  */
+
+import android.view.View
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Color
+
+val nodes : Int = 5
+val squares : Int = 3
+val parts : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4CAF50")
+val fillColor : Int = Color.parseColor("#212121")
