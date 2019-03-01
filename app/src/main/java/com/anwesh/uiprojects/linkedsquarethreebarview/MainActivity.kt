e5050c528264cf68d2578974c7fc69da5e9ef09b
Anwesh43 <anweshthecool0@gmail.com>
2019-03-02 02:22:31 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarethreebarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarethreebarview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquarethreebarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squarethreebarview.SquareThreeBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareThreeBarView.create(this)
     }
 }
