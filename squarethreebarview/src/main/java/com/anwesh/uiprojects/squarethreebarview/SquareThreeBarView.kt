0506ffa50f8581680c0a437b904c9a62a7bdcba9
Anwesh43 <anweshthecool0@gmail.com>
2019-03-02 01:52:28 +0530

    adding drawSTBNode canvas extension method

--- squarethreebarview/src/main/java/com/anwesh/uiprojects/squarethreebarview/SquareThreeBarView.kt
+++ squarethreebarview/src/main/java/com/anwesh/uiprojects/squarethreebarview/SquareThreeBarView.kt
@@ -19,7 +19,6 @@ val parts : Int = 2
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val sizeFactor : Float = 2.8f
-val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#4CAF50")
 val fillColor : Int = Color.parseColor("#212121")
 
@@ -29,3 +28,34 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawSTBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    val yGap : Float = (2 * size) / squares
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    save()
+    translate(0f, -size)
+    for (j in 0..(parts - 1)) {
+        val scj : Float = sc1.divideScale(j, parts)
+        save()
+        scale(1f - 2 * j, 1f)
+        for (k in 0..(squares - 1)) {
+            val sck : Float = scj.divideScale(k, squares)
+            save()
+            translate(0f, yGap * j)
+            drawRect(RectF(0f, 0f, size * sck, yGap), paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+    restore()
+}
